88a9afccf7282068c2a4f91263d92e96d6fc1b5f
smilingjb12 <smilingjb12@gmail.com>
2025-02-24 21:10:33 +0100

    Renamed CI job

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: process.env.NEXT_PUBLIC_CONVEX_URL.replace('https://', '') }],
+    remotePatterns: [{ hostname: process.env.NEXT_PUBLIC_CONVEX_URL?.replace('https://', '') }],
   },
 };
 
